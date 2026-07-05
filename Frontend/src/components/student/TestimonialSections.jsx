b2d9833d0b1ed791491207c7eaf7c8917b84f1da
VanshitSinghChauhan <vanshitsinghchauhan16032005@gmail.com>
2026-07-05 21:14:56 +0530

    Initial setup for folders

---
+++ Frontend/src/components/student/TestimonialSections.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestimonialSections = () => {
+  return (
+    <div>
+      <h1>TestimonialSections</h1>
+    </div>
+  )
+}
+
+export default TestimonialSections
