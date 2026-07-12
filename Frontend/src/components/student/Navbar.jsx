e87f8e42f4eafe54beb1cdae78f6a0637943f801
VanshitSinghChauhan <vanshitsinghchauhan16032005@gmail.com>
2026-07-12 22:39:44 +0530

    Create SideBar For Educator

--- Frontend/src/components/student/Navbar.jsx
+++ Frontend/src/components/student/Navbar.jsx
@@ -14,9 +14,9 @@ const Navbar = () => {
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
       <img onClick={()=>navigate('/')} src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
       <div className='hidden md:flex items-center gap-5 text-gray-500'>
-        <div className='flex items-center gap-5'>
+        <div className='flex items-center gap-5 '>
           {user && <>
-            <button onClick={()=>{navigate}}>{isEducator ? "Educator Dashboard": "Become Educator"}</button>
+            <button className='cursor-pointer' onClick={()=>{navigate('/educator')}}>{isEducator ? "Educator Dashboard" : "Become Educator"} </button>
             <Link to='/my-enrollments'>My Enrollments</Link>
           </>}
         </div>
@@ -30,7 +30,7 @@ const Navbar = () => {
       <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
         <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
           {user && <>
-            <button onClick={()=>{navigate}}>{isEducator ? "Educator Dashboard": "Become Educator"}</button>
+            <button onClick={()=>{navigate('/')}}>{isEducator ? "Educator Dashboard": "Become Educator"}</button>
             <Link to='/my-enrollments'>My Enrollments</Link>
           </>}
         </div>
