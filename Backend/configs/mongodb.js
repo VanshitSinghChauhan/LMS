4e4758bd1b453972b01f7824a8d9c379e26428b2
VanshitSinghChauhan <vanshitsinghchauhan16032005@gmail.com>
2026-07-15 23:10:58 +0530

    Create Backend

---
+++ Backend/configs/mongodb.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+//Connect to the mongo Db database
+
+const connectDB = async ()=>{
+    mongoose.connection.on('connected', ()=>console.log("DB Connected Successfully"))
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
+}
+
+export default connectDB
---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js'
+
+//Initialize Express
+const app = express()
+
+//Connect to Database
+await connectDB()
+
+//Middleware
+app.use(cors())
+
+//Route
+app.get('/', (req, res)=> res.send("API WORKING"))
+
+//Port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=>{console.log(`Server is Running on Port ${PORT}`)})
