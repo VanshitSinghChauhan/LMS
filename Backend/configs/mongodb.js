80310df988c17b0ab6b00fbec824a6359f8fc1e1
VanshitSinghChauhan <vanshitsinghchauhan16032005@gmail.com>
2026-07-16 17:07:14 +0530

    Create Clerk Webhook

--- Backend/configs/mongodb.js
+++ Backend/configs/mongodb.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 const connectDB = async ()=>{
     mongoose.connection.on('connected', ()=>console.log("DB Connected Successfully"))
     await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
+    //await mongoose.connect(process.env.MONGODB_URI, { dbName: 'lms' })
 }
 
 export default connectDB
