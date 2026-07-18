d15f0bff0023d8eb0fb7491bbfccecc573a6b2fe
VanshitSinghChauhan <vanshitsinghchauhan16032005@gmail.com>
2026-07-18 18:35:31 +0530

    Update controller and routes

--- Backend/controllers/educatorController.js
+++ Backend/controllers/educatorController.js
@@ -1,6 +1,7 @@
 import {clerkClient, getAuth} from '@clerk/express'
-import Course from '../models/Course'
+import Course from '../models/Course.js'
 import {v2 as cloudinary} from 'cloudinary'
+import { Purchase } from '../models/Purchase.js'
 
 //Update role to educator
 export const updatedRoletoEducator = async (req, res)=>{
@@ -22,22 +23,96 @@ export const addCourse = async(req, res)=>{
     try {
         const { courseData } = req.body
         const imageFile = req.file
-        const educatorId = getAuth(req)
+        const { userId } = getAuth(req) // this userId is educatorId
 
         if(!imageFile){
             return res.json({ status: false, message: 'Thumbnail not attached'})
         }
 
         const parsedCourseData = await JSON.parse(courseData)
-        parsedCourseData.educator = educatorId
+        parsedCourseData.educator = userId
         const newCourse = await Course.create(parsedCourseData)
         const imageUpload = await cloudinary.uploader.upload(imageFile.path)
 
-        newCourse.courseThubnail =  imageUpload.secure_url
+        newCourse.courseThumbnail = imageUpload.secure_url
         await newCourse.save()
 
-        res.json({ status: true, message: 'Course added'})
+        res.json({ success: true, message: 'Course added'})
     } catch (error) {
-        res.json({ status: false, message: error.message})
+        res.json({ success: false, message: error.message})
     }
+}
+
+//Get educator course
+export const getEducatorCourses = async(req, res)=>{
+    try {
+        const { userId } = getAuth(req) // userId is the educator
+        const courses = await Course.find({ educator: userId }) 
+        res.json({success: true, courses})
+    } catch (error) {
+        res.json({ success: false, message: error.message})
+    }
+}
+
+
+export const educatorDashboardData = async(req, res)=>{
+    try {
+        const { userId } = getAuth(req) // educator
+        const courses = await Course.find({ educator: userId }) 
+        const totalCourses = courses.length;
+
+        const courseIds = courses.map(course => course._id)
+
+        //Calculate ttal earning from Purchases
+        const purchase = await Purchase.find({
+            courseId: {$in: courseIds},
+            status: 'completed'
+        })
+
+        const totalEarnings = purchase.reduce((sum, purchase) => sum + purchase.amount, 0);
+
+        const enrolledStudentsData = [];
+        for(const course of courses){
+            const students = await User.find({
+                _id: {$in: course.enrolledStudents}
+            }, 'name imageUrl');
+
+            students.forEach(student => {
+                enrolledStudentsData.push({
+                    courseTitle: course.courseTitle,
+                    student
+                })
+            });
+        }
+
+        res.json({success: true, dashboardData: {
+            totalEarnings, enrolledStudentsData, totalCourses
+        }})
+    } catch (error) {
+        res.json({ success: false, message: error.message})
+    }
+}
+
+export const getEnrolledStudentsData = async (req, res) => {
+  try {
+    const { userId } = getAuth(req)
+    const courses = await Course.find({ educator: userId });
+    const courseIds = courses.map(course => course._id);
+
+    const purchases = await Purchase.find({
+      courseId: { $in: courseIds },
+      status: 'completed'
+    }).populate('userId', 'name imageUrl').populate('courseId', 'courseTitle');
+
+    const enrolledStudents = purchases.map(purchase =>({
+        student: purchase.userId,
+        courseTitle: purchase.courseId.courseTitle,
+        purchaseDate: purchase.createdAt
+    }))
+
+    res.json({success: true, enrolledStudents})
+
+  } catch (error) {
+    res.json({ success: false, message: error.message})
+  }
 }
--- Backend/routes/educatorRoutes.js
+++ Backend/routes/educatorRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { updatedRoletoEducator } from "../controllers/educatorController.js";
+import { addCourse, educatorDashboardData, getEducatorCourses, getEnrolledStudentsData, updatedRoletoEducator } from "../controllers/educatorController.js";
 import upload from "../configs/multer.js";
+import { protectEducator } from "../middlewares/authMiddleware.js";
 
 const educatorRouter = express.Router()
 
 educatorRouter.get('/update-role', updatedRoletoEducator)
-educatorRouter.post('/add-course', upload.single('image'))
+educatorRouter.post('/add-course', upload.single('image'), protectEducator, addCourse)
+educatorRouter.get('/courses', protectEducator, getEducatorCourses)
+educatorRouter.get('/dashboard', protectEducator, educatorDashboardData)
+educatorRouter.get('/enrolled-students', protectEducator, getEnrolledStudentsData)
 
 export default educatorRouter
